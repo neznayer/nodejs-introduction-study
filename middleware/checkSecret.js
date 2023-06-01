f02d8cfb1120d9b9efb6f892c208ba63a3fa18b3
Anton <nokker@gmail.com>
2023-06-01 18:28:05 +0900

    fix

--- middleware/checkSecret.js
+++ middleware/checkSecret.js
@@ -1,6 +1,6 @@
 function checkSecret(req, res, next) {
   const secret = req.headers["X-SECRET"];
-  if (secret === process.env.SECRET) {
+  if (secret === process.env.SECRET_KEY) {
     return next();
   }
   return res.status(401).send("Unauthorized");
