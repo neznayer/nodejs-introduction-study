295501c86137bf18bd1f6dcea6d45133c9e8e7e2
Anton <nokker@gmail.com>
2023-06-01 15:54:00 +0900

    add fs and middleware

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 const express = require("express");
-
+const fs = require("fs/promises");
 // Initialize express app
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
 
 // Define a simple GET route
 app.get("/", function (req, res) {
   return res.send("Hello World!");
 });
 
-const todos = [
-  { id: 1, title: "Learn Node.js", completed: false },
-  { id: 2, title: "Learn React.js", completed: false },
-  { id: 3, title: "Climb mount Fuji", completed: true },
-];
-
-app.get("/todos", function (req, res) {
+app.get("/todos", async function (req, res) {
   // get the todos list here,
+  const todos = JSON.parse(await fs.readFile("todos.json", "utf8"));
   return res.status(200).json(todos);
 });
 
-app.get("/todos/:id", function (req, res) {
+app.get("/todos/:id", async function (req, res) {
   // get the todo item by id
+  const todos = JSON.parse(await fs.readFile("todos.json", "utf8"));
+
   const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
   if (!todo) {
     return res.status(404).send("The todo item was not found");
   }
   return res.status(200).json(todo);
+});
+
+async function validateNewTodo(req, res, next) {
+  const newTodo = req.body;
+  if (!newTodo.title) {
+    return res.status(400).send("The title is required");
+  }
+  if (typeof newTodo.completed === "undefined") {
+    return res.status(400).send("The description is required");
+  }
+
+  return next();
+}
+
+app.post("/todos", validateNewTodo, async function (req, res) {
+  const newTodo = req.body;
+
+  const todos = JSON.parse(await fs.readFile("todos.json", "utf8"));
+  const newTodoId = todos.length + 1;
+  newTodo.id = newTodoId;
+  todos.push(newTodo);
+  try {
+    await fs.writeFile("todos.json", JSON.stringify(todos));
+  } catch (error) {
+    return res.status(500).send("Something went wrong");
+  }
+
+  return res.status(201).json(todos);
 });
 
 // Start the server
